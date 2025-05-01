1b5101e3a4665dfd906e4b1815cee4ade4d20623
Chaotic <124513444+Volburaal@users.noreply.github.com>
2025-05-01 19:15:25 +0500

    api debugging

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ interface PrivateRouteProps {
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const navigate = useNavigate();
   const token = isAuthenticated();
+  console.log(import.meta.env.VITE_API_BASE_URL)
 
   useEffect(() => {
     const checkAuth = () => {
