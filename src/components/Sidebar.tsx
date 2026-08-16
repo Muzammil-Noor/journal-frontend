dffda70c0e002edcc3e7b89ab8622b021980a469
Muzammil-Noor <muzammilnoor897@gmail.com>
2026-08-16 18:10:08 +0500

    Fix Build Error

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Menu, PanelLeftClose, PanelLeftOpen, X } from "lucide-react";
+import { PanelLeftClose, X } from "lucide-react";
 import type { LucideProps } from "lucide-react";
 import {
   Accordion,
